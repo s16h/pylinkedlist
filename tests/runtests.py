1d733e5955c741bb27ab72593165194d0d7a43f8
Shahriar Tajbakhsh <sh.tajbakhsh@gmail.com>
2015-02-03 23:30:51 +0000

    Made PYTHONPATH modification more general

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,7 +1,9 @@
-import unittest
+import os
 import sys
+import unittest
 
-sys.path.append('../')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, '..'))
 
 def make_and_get_suite(test_modules):
     suite = unittest.TestSuite()
